ae065b82bade61c4a94e33974171aa74eb6608c8
phellowpy <147276660+phellowpy@users.noreply.github.com>
2024-03-15 01:05:02 -0300

    att

---
+++ capitulo1.js
@@ -0,0 +1,34 @@
+//plano de fundo que muda
+
+document.getElementById('changeBackgroundBtn1').addEventListener('mouseenter', function() {
+    document.body.style.backgroundImage = "url('img/capitulo1/inicial/personagens.jpg')";
+  });
+  
+  document.getElementById('changeBackgroundBtn1').addEventListener('mouseleave', function() {
+    document.body.style.backgroundImage = "url('img/inicial/home.webp')";
+  });
+  
+  document.getElementById('changeBackgroundBtn2').addEventListener('mouseenter', function() {
+    document.body.style.backgroundImage = "url('img/capitulo1/inicial/historia.jpg')";
+  });
+  
+  document.getElementById('changeBackgroundBtn2').addEventListener('mouseleave', function() {
+    document.body.style.backgroundImage = "url('img/inicial/home.webp')";
+  });
+  
+  document.getElementById('changeBackgroundBtn3').addEventListener('mouseenter', function() {
+    document.body.style.backgroundImage = "url('img/capitulo1/inicial/mecanicas.png')";
+  });
+  
+  document.getElementById('changeBackgroundBtn3').addEventListener('mouseleave', function() {
+    document.body.style.backgroundImage = "url('img/inicial/home.webp')";
+  });
+  
+  document.getElementById('changeBackgroundBtn4').addEventListener('mouseenter', function() {
+    document.body.style.backgroundImage = "url('img/capitulo1/inicial/fitas.png')";
+  });
+  
+  document.getElementById('changeBackgroundBtn4').addEventListener('mouseleave', function() {
+    document.body.style.backgroundImage = "url('img/inicial/home.webp')";
+  });
+  
